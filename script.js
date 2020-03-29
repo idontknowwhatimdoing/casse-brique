787621a1cb7d35e161982c7ee4ab9efbf08e9bb0
jeremy <jeyx.22@gmail.com>
2020-03-29 20:30:52 +0200

    skeleton of the programm

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
 "use strict";
 function setup() {
+    // create_ball();
+    // create_bricks();
+    // create_paddle();
 }
 function update() {
+    // move_ball();
+    // bounce();
+    // move_paddle();
+    // destroy_bricks();
 }
 function render() {
+    // draw_ball();
+    // draw_bricks();
+    // draw_paddle();
 }
